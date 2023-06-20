57a926c0882eef5b728db031059d28783ef5a00c
Alex <118717896+Lexylent@users.noreply.github.com>
2023-06-20 18:22:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from flask import Flask, jsonify, render_template, request, send_from_directory
+from server_control import stop_previous_instance, start_new_instance
 import webbrowser
 from audio import record_audio, transcribe_audio
 from api_service import generate_answer
 from database import create_tables, insert_recording
 from garbage_collector import start_garbage_collector
+
+# Portnummer, die Sie verwenden möchten
+port = 5000
+
+# Überprüfen und Beenden der vorherigen Instanz
+stop_previous_instance(port)
+
+# Starten der neuen Instanz
+start_new_instance(port)
 
 app = Flask(__name__)
 
@@ -11,6 +21,39 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+# Setze den Flask-Server als globalen Thread
+flask_server_thread = None
+
+
+#Diese Funktion wird aufgerufen, wenn eine POST-Anfrage an /exit gesendet wird.
+#Sie beendet den Flask-Server und gibt den Text "Programm beendet" zurück.
+
+
+@app.route('/exit', methods=['POST'])
+def exit_program():
+    
+    print("Das Programm wurde beendet")
+    shutdown_flask_server()  # Flask-Server beenden
+    return 'Programm beendet'  # Textausgabe
+
+
+# Diese Funktion startet den Flask-Server auf Port 5000.
+
+def run_flask_server():
+    
+    app.run(port=5000)
+
+def shutdown_flask_server():
+    
+   # Diese Funktion wird verwendet, um den Flask-Server ordnungsgemäß zu beenden.
+    
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func is not None:
+        func()  # Ruft die Funktion zum Beenden des Flask-Servers auf
+
+
 
 # Route zum Transkribieren des aufgenommenen Audios
 @app.route('/transcribe', methods=['POST'])
@@ -55,9 +98,16 @@ def serve_css():
 def serve_script():
     return send_from_directory('templates', 'scripts.js')
 
+
+# Starten der neuen Instanz
+start_new_instance(port)
+
+
+
+
 # Öffne einen Tab im Browser und starte die App
 if __name__ == '__main__':
     create_tables()  # Tabellen in der Datenbank erstellen
     start_garbage_collector()  # Startet den garbage_collector in einem separaten Thread.
     webbrowser.open('http://127.0.0.1:5000')
-    app.run(port=5000)
+    app.run(port=5000, debug=False)
