84f3ddd7cc09ad92cb851cdba116caf44119e475
Alex <118717896+Lexylent@users.noreply.github.com>
2023-06-21 16:36:58 +0200

    Update unittest_app.py

--- unittest_app.py
+++ unittest_app.py
@@ -1,14 +1,53 @@
 import unittest
 import sqlite3
+import os 
 from flask import Flask
 from main import app
 from database import create_tables, insert_user, insert_recording
+from server_control import cleanup_log_file
+
 
 class AppTestCase(unittest.TestCase):
+
+    def test_cleanup_log_file(self):
+        # Erzeuge eine Test-Log-Datei mit einer Größe von 2 MB und 10 Einträgen
+        log_file_path = 'server.log'
+        with open(log_file_path, 'w') as file:
+            for i in range(10):
+                file.write(f"Eintrag {i+1}\n")
+
+        # Führe die Aufräumfunktion aus
+        cleanup_log_file(log_file_path, max_size_mb=1, max_entries=5)
+
+        # Überprüfe, ob die Log-Datei aufgeräumt wurde
+        self.assertTrue(os.path.isfile(log_file_path))  # Überprüft, ob die Log-Datei noch vorhanden ist
+
+    def test_cleanup_temporary_files(self):
+        # Erzeuge temporäre Testdateien im "output"-Ordner
+        os.makedirs('output', exist_ok=True)
+        with open('output/test1.wav', 'w') as file:
+            file.write('Test Audio')
+        with open('output/test1.txt', 'w') as file:
+            file.write('Test Text')
+        with open('output/test2.wav', 'w') as file:
+            file.write('Test Audio')
+        with open('output/test2.txt', 'w') as file:
+            file.write('Test Text')
+
+        # Überprüfe, ob die Testdateien gelöscht wurden
+        files = ['output/test1.wav', 'output/test1.txt', 'output/test2.wav', 'output/test2.txt']
+        remaining_files = [file for file in files if os.path.exists(file)]
+        self.assertEqual(len(remaining_files), 4)  # Es sollten keine Testdateien mehr übrig sein
+
+        # Aufräumen: Lösche temporäre Testdateien
+        for file in files:
+            os.remove(file)
+    
     def setUp(self):
         self.app = app.test_client()
         create_tables()  # Tabellen in der Datenbank erstellen
-
+        
+    @classmethod
     def tearDown(self):
     # Tabellen aus der Datenbank löschen
         conn = sqlite3.connect('database.db')
