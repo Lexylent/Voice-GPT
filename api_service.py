0e1972bcb84dabf2b44055a3757351d977128761
Alex <118717896+Lexylent@users.noreply.github.com>
2023-06-23 10:38:00 +0200

    Update api_service.py

--- api_service.py
+++ api_service.py
@@ -1,27 +1,32 @@
 import openai
 
-openai.api_key ='Hier_kommt_der-apikey_hinein' #CHATGPT KEY
+openai.api_key ='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' #CHATGPT KEY
 
 # Versuche, eine Anfrage an OpenAI zu stellen
 def generate_answer(question):
     try:
+        # Der Text, der den Generator informiert, dass es sich um eine Korrekturaufgabe handelt
         prompt = f"Frage: {question}\nAntwort:"
-        max_tokens = 100
 
+        # Die maximale Anzahl von Tokens (Texteinheiten) für die generierte Antwort
+        max_tokens = 200
+
+        # Aufruf der OpenAI API, um eine Textgenerierung durchzuführen
         response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=prompt,
-            max_tokens=max_tokens,
-            n=1,
-            stop="Antwort:",
-            temperature=0.5,
-            top_p=1.0,
-            frequency_penalty=0.0,
-            presence_penalty=0.0
+            engine="text-davinci-003",  # Die gewählte Textgenerierungs-Engine
+            prompt=prompt,  # Der vorbereitete Text, der den Generator instruiert
+            max_tokens=max_tokens,  # Die maximale Länge der generierten Antwort
+            n=1,  # Anzahl der gewünschten generierten Antworten (in diesem Fall nur eine)
+            stop="Antwort",  # Es wird kein spezielles Stopp-Token verwendet, um das Ende der Antwort anzugeben
+            temperature=0.5,  # Ein Maß für die Zufälligkeit der generierten Antwort
+            top_p=1.0,  # Anteil der Wahrscheinlichkeiten der nächsten Token, die berücksichtigt werden sollen
+            frequency_penalty=0.0,  # Strafe für die Verwendung bereits verwendeter Tokens basierend auf ihrer Häufigkeit
+            presence_penalty=0.0  # Strafe für die Verwendung bereits verwendeter Tokens basierend auf ihrer Anwesenheit
         )
+        
         # Überprüfe, ob eine Antwort vorhanden ist  
         if 'choices' in response and len(response['choices']) > 0:
-            # Extrahiere den Text der ersten Antwort und entferne Leerzeichen am Anfang und End
+            # Extrahiere den Text der ersten Antwort und entferne Leerzeichen am Anfang und Ende
             return response['choices'][0]['text'].strip()
         else:
             # Falls keine Antwort gefunden wurde, gib einen entsprechenden Text zurück
@@ -36,7 +41,6 @@ def generate_answer(question):
         # Allgemeine Fehlerbehandlung
         print("Ein Fehler ist aufgetreten:", str(e))
         return 'Ein unerwarteter Fehler ist aufgetreten.'
-
 
 
 
