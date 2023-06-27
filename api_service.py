a041c324a6208261d93d69c77730d2c11bbfb523
Alex <118717896+Lexylent@users.noreply.github.com>
2023-06-27 19:23:37 +0200

    Update api_service.py
    Konversation beibehalten.

--- api_service.py
+++ api_service.py
@@ -1,33 +1,56 @@
 import openai
+import json
 
-openai.api_key ='Hier_kommt_der_openai_api_key' #CHATGPT KEY
+openai.api_key = 'Hier_kommt_der_apikey_hinein'  # CHATGPT KEY
+
+# Vorheriger Konversationsverlauf
+previous_conversation = []
 
 # Versuche, eine Anfrage an OpenAI zu stellen
 def generate_answer(question):
+    global previous_conversation  # Deklariere die Variable als global
+
     try:
-        # Der Text, der den Generator informiert, dass es sich um eine Korrekturaufgabe handelt
-        prompt = f"Frage: {question}\nAntwort:"
+        # Aufbau des Konversationsverlaufs
+        conversation = previous_conversation.copy()
+        conversation.append(question)   # Benutzereintrag mit der gestellten Frage
 
         # Die maximale Anzahl von Tokens (Texteinheiten) für die generierte Antwort
-        max_tokens = 200
+        max_tokens = 400
+        
+
+        # Konvertiere den Konversationsverlauf in einen JSON-String
+        prompt = json.dumps(conversation)
 
         # Aufruf der OpenAI API, um eine Textgenerierung durchzuführen
         response = openai.Completion.create(
             engine="text-davinci-003",  # Die gewählte Textgenerierungs-Engine
-            prompt=prompt,  # Der vorbereitete Text, der den Generator instruiert
+            prompt=prompt,  # Der Konversationsverlauf als Prompt
             max_tokens=max_tokens,  # Die maximale Länge der generierten Antwort
             n=1,  # Anzahl der gewünschten generierten Antworten (in diesem Fall nur eine)
-            stop="Antwort",  # Es wird kein spezielles Stopp-Token verwendet, um das Ende der Antwort anzugeben
-            temperature=0.5,  # Ein Maß für die Zufälligkeit der generierten Antwort
+            temperature=0.8,  # Ein Maß für die Zufälligkeit der generierten Antwort
             top_p=1.0,  # Anteil der Wahrscheinlichkeiten der nächsten Token, die berücksichtigt werden sollen
             frequency_penalty=0.0,  # Strafe für die Verwendung bereits verwendeter Tokens basierend auf ihrer Häufigkeit
             presence_penalty=0.0  # Strafe für die Verwendung bereits verwendeter Tokens basierend auf ihrer Anwesenheit
         )
-        
-        # Überprüfe, ob eine Antwort vorhanden ist  
+
+        # Überprüfe, ob eine Antwort vorhanden ist
         if 'choices' in response and len(response['choices']) > 0:
             # Extrahiere den Text der ersten Antwort und entferne Leerzeichen am Anfang und Ende
-            return response['choices'][0]['text'].strip()
+            answer = response['choices'][0]['text'].strip()
+            answer_text = answer.split('{"role": "assistant", "content": ')[-1].strip('"}')
+
+            print(answer_text)
+
+            # Konversationsverlauf aktualisieren
+            conversation.append({"role": "assistant", "content": answer_text})
+            previous_conversation = conversation  # Speichern des aktualisierten Konversationsverlaufs
+
+            return answer_text
+
+
+
+
         else:
             # Falls keine Antwort gefunden wurde, gib einen entsprechenden Text zurück
             return 'Keine Antwort gefunden.'
@@ -41,11 +64,3 @@ def generate_answer(question):
         # Allgemeine Fehlerbehandlung
         print("Ein Fehler ist aufgetreten:", str(e))
         return 'Ein unerwarteter Fehler ist aufgetreten.'
-
-
-
-
-
-
-
-
